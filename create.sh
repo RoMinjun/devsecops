d0b8db5158a73b8249b6c4819ecd8f4eae97d641
RoMinjun <rominkleeman@hotmail.com>
2025-09-06 18:45:57 +0200

    split master/worker.yml

--- create.sh
+++ create.sh
@@ -7,9 +7,18 @@ KEY_NAME=${1:-vockey}
 aws cloudformation create-stack --stack-name k3s-vpc --template-body file://k3s-vpc.yml --region "$REGION"
 aws cloudformation wait stack-create-complete --stack-name k3s-vpc --region "$REGION"
 
-aws cloudformation create-stack --stack-name k3s-cluster \
+# Master stack (exports SG + master IP)
+aws cloudformation create-stack --stack-name k3s-master \
   --capabilities CAPABILITY_IAM \
-  --template-body file://k3s-cluster.yml \
+  --template-body file://master.yml \
   --parameters ParameterKey=KeyName,ParameterValue=$KEY_NAME \
   --region "$REGION"
-aws cloudformation wait stack-create-complete --stack-name k3s-cluster --region "$REGION"
+aws cloudformation wait stack-create-complete --stack-name k3s-master --region "$REGION"
+
+# Worker stack (imports master exports)
+aws cloudformation create-stack --stack-name k3s-worker \
+  --capabilities CAPABILITY_IAM \
+  --template-body file://worker.yml \
+  --parameters ParameterKey=KeyName,ParameterValue=$KEY_NAME \
+  --region "$REGION"
+aws cloudformation wait stack-create-complete --stack-name k3s-worker --region "$REGION"
--- delete.sh
+++ delete.sh
@@ -3,8 +3,11 @@ set -e
 
 REGION="us-east-1"
 
-aws cloudformation delete-stack --stack-name k3s-cluster --region "$REGION"
-aws cloudformation wait stack-delete-complete --stack-name k3s-cluster --region "$REGION"
+aws cloudformation delete-stack --stack-name k3s-worker --region "$REGION"
+aws cloudformation wait stack-delete-complete --stack-name k3s-worker --region "$REGION"
+
+aws cloudformation delete-stack --stack-name k3s-master --region "$REGION"
+aws cloudformation wait stack-delete-complete --stack-name k3s-master --region "$REGION"
 
 aws cloudformation delete-stack --stack-name k3s-vpc --region "$REGION"
 aws cloudformation wait stack-delete-complete --stack-name k3s-vpc --region "$REGION"
